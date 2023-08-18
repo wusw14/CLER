8ff5a2eea8ccc144201660a4603f64e5d4635f22
wusw14 <wushw.18@pku.edu.cn>
2023-08-18 22:16:32 +0800

    delete irrelevant files

--- run.sh
+++
@@ -1,4 +0,0 @@
-nohup python -u run.py AB 0 500 >> logs/AB5.log 2>&1 &
-nohup python -u run.py AB 1 1000 >> logs/AB10.log 2>&1 &
-nohup python -u run.py AB 2 1500 >> logs/AB15.log 2>&1 &
-nohup python -u run.py AB 3 2000 >> logs/AB20.log 2>&1 &
